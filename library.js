4940221e3b956b9cd8469e4f4a8136aefc0046e8
Alexkin Skywalker <alexkin_skywalker@hotmail.com>
2020-02-14 13:47:51 -0600

    OpenID setup.
    OpenID setup.

--- library.js
+++ library.js
@@ -2,7 +2,10 @@
 	'use strict';
     /* globals module, require */
 
-    var passport = module.parent.require('passport'),
+    var user = require.main.require('./src/user'),
+        meta = require.main.require('./src/meta'),
+        db = require.main.require('./src/database')
+        passport = module.parent.require('passport'),
         nconf = module.parent.require('nconf'),
         winston = module.parent.require('winston'),
         { Issuer, Strategy, custom } = require('openid-client'),
@@ -29,10 +32,11 @@
 
     /**
     *   Initializes the plugin
+    *
     *   @param {Object} data
     *   @param {Function} callback
     */
-    Oidc.init = function (params, callback) {
+    Oidc.init = function(params, callback) {
 		winston.verbose('Setting up OpenID Connect UI routes...');
 
         var hostHelpers = require.main.require('./src/routes/helpers');
@@ -66,7 +70,128 @@
 		callback();
     };
 
-    Oidc.addMenuItem = function (custom_header, callback) {
+    Oidc.getSettings = function(callback) {
+		if (Oidc.settings) {
+			return callback();
+		}
+
+		meta.settings.get('sso-oidc', (err, settings) => {
+			Oidc.settings = settings;
+			callback();
+		});
+    }
+
+    /**
+    *   Creates the Passport Strategy to login with OpenID Connect.
+    *
+    *   @param {Array} strategies
+    *   @param {Function} callback
+    */
+    Oidc.getStrategy = function(strategies, callback) {
+        if (!Oidc.settings) {
+			return Oidc.getSettings(function () {
+				Oidc.getStrategy(strategies, callback);
+			});
+        }
+
+        if (
+            Oidc.settings !== undefined &&
+            Oidc.settings.hasOwnProperty('discover_url') && Oidc.settings.discover_url &&
+            Oidc.settings.hasOwnProperty('client_id') && Oidc.settings.client_id &&
+            Oidc.settings.hasOwnProperty('client_secret') && Oidc.settings.client_secret
+        ) {
+            //To see Oidc.settings:
+            winston.info('Oidc.settings:');
+            winston.info(JSON.stringify(Oidc.settings));
+
+            winston.verbose('[oidc] Fetching OpenID Connect Issuer informations ...');
+            const issuer = await Issuer.discover(Oidc.settings.discover_url);
+            winston.verbose('[oidc] Creating OpenID Connect Passport Strategy ...');
+
+            const client = new issuer.Client({
+                client_id: Oidc.settings.client_id,
+                client_secret: Oidc.settings.client_secret,
+                redirect_uris: [nconf.get('url') + AUTH_OIDC_CALLBACK_PATH],
+            });
+
+            // Adding some timestamp tolerance as they may be a little different
+            // if nodebb and the OpenID Connect server are on two separate
+            // hosts.
+            client[custom.clock_tolerance] = CLOCK_TOLERANCE;
+
+            //To see client:
+            winston.info('client:');
+            winston.info(JSON.stringify(client));
+
+            const strategy = new Strategy({
+                client,
+                params: {
+                    // In OpenID Connect,
+                    // => issuer and subject in the 'openid' scope
+                    // => email in the 'email' scope
+                    // => username in the 'profile' scope ( as 'preferred_username' )
+                    scope: 'openid email profile'
+                },
+            }, function (tokenSet, profile, callback) {
+                winston.info('Hasta aquí es en \'verify\'');
+                winston.info(JSON.stringify(profile));
+                winston.verbose('Verifing after SSO response...');
+                try {
+                    const claims = tokenSet.claims();
+                    let uid = await Oidc.getDbObjectField('oidc:uid', hash(claims.iss, subject));
+
+                    if (config.matchUserByEmail && !uid) {
+                        winston.verbose('No user found, but checking if an user with the same email exists...');
+                        uid = await Oidc.getUidByEmail(claims.email);
+                        if (uid) {
+                            winston.verbose('Found corresponding user, merging...');
+                            await Oidc.merge(uid, {
+                                oidcIssuer: claims.iss,
+                                oidcSubject: claims.sub,
+                            });
+                        }
+                    }
+
+                    if (uid) {
+                        winston.verbose(`Found user with uid '${uid}'.`);
+                        callback(null, { uid });
+                        return;
+                    }
+
+                    winston.verbose(`User not found, creating new one.`);
+                    uid = await Oidc.create({
+                        username: claims.preferred_username || claims.email,
+                        email: String(claims.email).toLowerCase(),
+                        oidcIssuer: claims.iss,
+                        oidcSubject: claims.sub,
+                    });
+
+                    callback(null, { uid });
+                } catch (err) {
+                    callback(err);
+                }
+            });
+
+            //To see strategy:
+            winston.info('strategy:');
+            winston.info(JSON.stringify(strategy));
+
+            passport.use(strategy.name, strategy);
+        }
+
+        strategies.push({
+            name: strategy.name,
+            url: AUTH_OIDC_LOGIN_PATH,
+            callbackURL: AUTH_OIDC_CALLBACK_PATH,
+            icon: 'fa-openid',
+            scope: 'openid email profile',
+        });
+
+        winston.verbose('[oidc] Strategy initialized ...');
+        return strategies;
+	};
+
+    Oidc.addMenuItem = function(custom_header, callback) {
 		custom_header.authentication.push({
 			'route': constants.admin.route,
 			'icon': constants.admin.icon,
@@ -74,7 +199,55 @@
 		});
 
 		callback(null, custom_header);
-	};
+    };
+    
+    Oidc.getDbObjectField = async function(key, field) {
+        return new Promise((resolve, reject) => {
+            db.getObjectField(key, field, (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    }
+
+    Oidc.getUidByEmail = async function(email) {
+        return user.getUidByEmail(email.toLocaleLowerCase());
+    }
+
+    Oidc.merge = async function(uid, oidc) {
+        const oidchash = hash(oidc.oidcIssuer, oidc.oidcSubject);
+
+        await user.setUserField(uid, 'oidcissuer', oidc.oidcIssuer);
+        await user.setUserField(uid, 'oidcsubject', oidc.oidcSubject);
+        await setDbObjectField('oidc:uid', oidchash, uid);
+    }
+
+    Oidc.create = async function(userData) {
+        const uid = await new Promise((resolve, reject) => {
+            user.create({
+                username: userData.username,
+                email: userData.email,
+            }, (err, uid) => {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(uid);
+            });
+        });
+    
+        const oidchash = hash(userData.oidcIssuer, userData.oidcSubject);
+    
+        await User.setUserField(uid, 'oidcissuer', userData.oidcIssuer); // for display
+        await User.setUserField(uid, 'oidcsubject', userData.oidcSubject); // for display
+    
+        // Saving
+        await setDbObjectField('oidc:uid', oidchash, uid);
+    
+        return uid;
+    }
 
 	module.exports = Oidc;
 }(module));
--- templates/admin/plugins/sso-oidc.tpl
+++ templates/admin/plugins/sso-oidc.tpl
@@ -1,48 +1,41 @@
 <div class="row">
 	<div class="col-xs-12">
 		<div class="panel panel-default">
-			<div class="panel-heading">Facebook Social Authentication</div>
+			<div class="panel-heading">OpenID Connect Authentication</div>
 			<div class="panel-body">
 				<p>
-					Create a <strong>Facebook Application</strong> via the
-					<a href="https://developers.facebook.com/apps/">Facebook Developers Page</a> and
-					then paste your application details here.
+					Use your <strong>OpenID Connect Client</strong> data
+					from the OpenID Connect server and then paste your
+					application details here.
 				</p>
 				<ul>
 					<li>
-						You will need to enable <strong>"Client OAuth Login"</strong> and <strong>"Web OAuth Login"</strong>
-					from the "Facebook Login" product
-					</li>
-					<li>
-						You will need to paste <code>{baseUrl}/auth/facebook/callback</code> into the
-						<strong>"Valid OAuth Redirect URIs"</strong> field.
+						You will need to paste <code>{baseUrl}/auth/oidc/callback</code> into the
+						<strong>"Redirect URIs"</strong> field on your OpenID Connect Server.
 					</li>
 				</ul>
-				</p>
-				<form role="form" class="sso-facebook-settings">
+				<form role="form" class="sso-oidc-settings">
 					<div class="form-group">
-						<label for="app_id">Application ID</label>
-						<input type="text" id="app_id" name="app_id" title="Application ID" class="form-control" placeholder="Application ID"><br />
+						<label for="discover_url">Discover URL</label>
+						<input type="text" id="discover_url" name="discover_url" title="Discover URL" class="form-control" placeholder="Discover URL">
 					</div>
 					<div class="form-group">
-						<label for="secret">Secret</label>
-						<input type="text" id="secret" name="secret" title="Secret" class="form-control" placeholder="Secret">
+						<label for="app_id">Client ID</label>
+						<input type="text" id="client_id" name="client_id" title="Client ID" class="form-control" placeholder="Client ID"><br />
+					</div>
+					<div class="form-group">
+						<label for="client_secret">Client Secret</label>
+						<input type="text" id="client_secret" name="client_secret" title="Client Secret" class="form-control" placeholder="Client Secret">
 					</div>
 					<div class="checkbox">
-						<label for="autoconfirm" class="mdl-switch mdl-js-switch mdl-js-ripple-effect">
-							<input type="checkbox" class="mdl-switch__input" id="autoconfirm" name="autoconfirm" />
-							<span class="mdl-switch__label">Skip email verification for people who register using SSO?</span>
-						</label>
-					</div>
-					<div class="checkbox">
-						<label for="disableRegistration" class="mdl-switch mdl-js-switch mdl-js-ripple-effect">
-							<input type="checkbox" class="mdl-switch__input" id="disableRegistration" name="disableRegistration" />
-							<span class="mdl-switch__label">Disable user registration via SSO</span>
+						<label for="matchUser" class="mdl-switch mdl-js-switch mdl-js-ripple-effect">
+							<input type="checkbox" class="mdl-switch__input" id="matchUser" name="matchUser" />
+							<span class="mdl-switch__label">Match User by Email</span>
 						</label>
 					</div>
 					<p class="help-block">
 						Restricting registration means that only registered users can associate their account with this SSO strategy.
-						This restriction is useful if you have users bypassing registration controls by using social media accounts, or
+						This restriction is useful if you have users bypassing registration controls by using OpenID Connect accounts, or
 						if you wish to use the NodeBB registration queue.
 					</p>
 				</form>
